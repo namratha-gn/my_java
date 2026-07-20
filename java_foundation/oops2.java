fd667122a9123c8f78f83238441919201f0f7d06
namratha-gn <namrathagn94@gmail.com>
2026-07-20 12:43:48 +0530

    Added OOP notes

---
+++ java_foundation/oops2.java
@@ -0,0 +1,22 @@
+// why oops==>to store the numbers (same  data type==>integer array),,, to store names==> string type array,,,To strore name(string),marks(int),rollnumber(float) all at once===> use class
+// class==>named group of properties and methods
+//         class is a blueprint(that has some basic properties(but values are different ) and performs some functions) and using this blueprint different objects are created
+//             -object is the insrance of the class(instance means the physical stuff of the class)
+//             -object is the physical stuff that actually exist not just the blueprint
+//             -object actually occupies the memory
+//             -object has 
+//                1.state=its own value from its datatype
+//                2.Identity = how one object is   different from other(interns of where it is stored)
+//         EXAMPLE:
+//         Car is the class(blue print)
+//         - properties = engine, price, seats
+//         - performs different functions
+
+//         Lamborgini(object created from class)
+//         - values = petrol engine, $20000, 5
+//         -performs one function
+
+//         Banz(object created from class)
+//         - values = diezel engine, $80000, 7
+//         - performs another function
+//         [SEE THE PICTURE: class example]
