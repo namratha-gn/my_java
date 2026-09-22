686460c0d85d951bbf928168a1eee7c46f776b80
namratha-gn <namrathagn94@gmail.com>
2026-09-22 23:51:43 +0530

    updated
    updated

--- java_foundation/basics.java
+++ java_foundation/basics.java
@@ -67,9 +67,7 @@
 // Array	arr.length ✅
 
 // In javaScript str[0]...In java str.charAt(0)-'0'
- 
 // Here str.charAt(0) is a character ....you cannot comapre this character with ,any integer (if you wnat in any situation) ..soo str.charAt(0)-'0'...this internally comapares thier unicodes and gives output a integer 
-
 // if sound=true ..break;(this situation may exits in many conditions)...true to use this found kin dof avriables in your code
 
 // one very very vital thing that one can know is......
